ba0dae312bf02e7a9a717e90c1c821c382661938
Esther Oluyemi <estheroluyemi25@gmail.com>
2026-09-02 03:02:17 +0100

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { Navbar} from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Experience } from './components/experience/experience';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [Navbar, Hero, About, Skills, Projects, Experience, Contact, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('my-portfolio');
+}
