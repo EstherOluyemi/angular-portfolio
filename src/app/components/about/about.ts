8bef9bbb8e1b3abd7ac4eff51be38b08dd42751e
Esther Oluyemi <estheroluyemi25@gmail.com>
2026-09-04 19:34:20 +0100

    corrected the about section icons

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
 
+import {
+  LucideBookOpen,
+  LucideCloud,
+  LucideCode2,
+  LucideMonitorSmartphone,
+} from '@lucide/angular';
+
 @Component({
-  imports: [],
+  standalone: true,
+
+  imports: [
+    LucideCode2,
+    LucideCloud,
+    LucideMonitorSmartphone,
+    LucideBookOpen,
+  ],
+
   selector: 'app-about',
+
   styleUrl: './about.css',
+
   templateUrl: './about.html',
 })
+
 export class About {}
